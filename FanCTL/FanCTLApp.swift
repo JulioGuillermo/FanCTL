6e583ab9370092e40277c324287ca320b88f99d5
Julio Guillermo Mayo Vidal <julioguillermo0802@gmail.com>
2026-08-15 11:57:19 +0200

    Initial Commit

---
+++ FanCTL/FanCTLApp.swift
@@ -0,0 +1,17 @@
+//
+//  FanCTLApp.swift
+//  FanCTL
+//
+//  Created by Julio Guillermo Mayo Vidal on 15/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct FanCTLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
